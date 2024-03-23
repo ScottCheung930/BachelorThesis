f52191bf7cb83ff6f101bdb68b427ad7e9c46948
ScottCheung930 <scottcheung@zju.edu.cn>
2024-03-23 19:20:46 +0100

    2024/3/23

--- MUSICforOFDMsensing.m
+++ MUSICforOFDMsensing.m
@@ -10,45 +10,12 @@ global  NR
 [d,ind_D] = sort(diag(D),'descend');
 L=MDL(d);
 U_n=V(:,ind_D(L+1:end));
-theta=linspace(-pi/3,pi/3);
+theta=linspace(-pi/6,pi/6);
 A=steeringGen(theta, NR);
 P_music_theta=zeros(1,length(theta));
 for ii=1:length(theta)
     P_music_theta(ii)=1/(A(:,ii)'*(U_n*U_n')*A(:,ii));
 end
-
-
-
-%% range estimation
-% [V,D]=eig(R);
-% 
-% [~,ind_D] = sort(diag(D),'descend');
-% U_n = V(:,ind_D(k+1:end));
-% 
-% delay = linspace(0,2*100/c0*delta_f,M);
-% ii = 0:M-1;
-% A = exp(-1j*2*pi*kron(ii',delay));
-% P_music_range = zeros(size(A,2),1);
-% for jj = 1:size(A,2)
-%     P_music_range(jj) = 1/(A(:,jj)'*(U_n*U_n')*A(:,jj));
-% end
-
-
-%% Velocity Estimation
-% R_dop = CIM.'*conj(CIM)/M;
-% [V,D]=eig(R_dop);
-% [~,ind_D] = sort(diag(D),'descend');
-% U_n = V(:,ind_D(k+1:end));
-% 
-% doppler = linspace(0,2*100/lambda*Ts,M);
-% ii = 0:N-1;
-% A = exp(1j*2*pi*kron(ii',doppler));
-% P_music_velo = zeros(size(A,2),1);
-% for jj = 1:size(A,2)
-%     P_music_velo(jj) = 1/(A(:,jj)'*(U_n*U_n')*A(:,jj));
-% end
-
-
 end
 
 %% find the number of targets using MDL criterion
--- ofdm_radar_main.m
+++ ofdm_radar_main.m
@@ -7,9 +7,9 @@ global c0 fc lambda M N delta_f Ts CPsize NR NT; % 定义全局变量
 %% Transmitter
 % System parameters
 c0 = 3e+8;  % 光速(m/s)
-fc = 30e+9;  % 载波频率，30GHz
+fc = 28e+9;  % 载波频率，28GHz
 lambda = c0 / fc; % 波长(1cm)
-M = 1024;     % 子载波数（每个subframe的）
+M = 3168;     % 子载波数（每个subframe的）
 N = 112;       % subframe长度=N*符号长度
 delta_f = 120e+3; % 子载波间隔，120kHz
 T = 1 / delta_f; % 符号长度，8.3us
@@ -18,6 +18,7 @@ Ts = T + Tcp;  % 总符号时延=符号持续时间+循环前缀
 CPsize = M / 4; % 循环前缀码的长度，子载波数量的1/4
 NR = 10; %接收天线数（与发射天线相等）
 NT = 10; %发射天线数
+Fp=10;
 bitsPerSymbol = 4; % 每符号比特数
 qam = 2^(bitsPerSymbol); % QAM调制，每正交方向4个幅度取值，使用16-QAM
 
@@ -31,17 +32,17 @@ for ii=1:NT
     Tx(1:M,1:N,ii)=TxData;
 end
 %% Channel
-SNR = 30;
-r = [10,80,20,10];  % range.(it can be a row vector)
-v = [0,10,-15,20];  %velocity
-theta = [-pi/4, -pi/6, pi/12,pi/6];
+SNR = 20;
+r = [25,10, 20];  % range.(it can be a row vector)
+v = [20, 40 ,30];  %velocity
+theta = [-pi/12, -pi/9, pi/12];
 L=length(theta);
 Ar=steeringGen(theta,NR);% steering matrix of receiver
 At=steeringGen(theta,NT);% steering matrix of transmitter
 Beta=coefGen(r,v); % phase shift by doppler and delay 
 
 %% Receiver
-Rx=zeros(M,N,NR);
+Rx=zeros(M,N,NR);% Rx(m,n,:)为第m个子载波，第n个OFDM符号被NR个接收天线接收的数据向量
 P_symbol=0;
 for ii=1:M
     P_symbol_ii=0;
@@ -54,6 +55,8 @@ for ii=1:M
     P_symbol_ii=P_symbol_ii/N;
     P_symbol=P_symbol+P_symbol_ii;
 end
+
+% Add noise
 P_symbol=P_symbol/M;%平均每个天线单元的接收符号功率
 N0=P_symbol*10^(-SNR/10);%N0：单边噪声功率
 for ii=1:M
@@ -66,12 +69,20 @@ for ii=1:M
     end
 end
 
+% receive symbols
+RxData=zeros(M,N);
+for ii=1:M
+    for jj=1:N
+            RxData(ii,jj)=sum(Rx(ii,jj,1:NR));
+    end
+end
+
 %% Sample Covariance Matrix
 R=zeros(NR,NR);
 for ii=1:M
     R0=zeros(NR,NR);
     for jj=1:N
-       y=reshape(Rx(ii,jj,1:NR),10,1);
+       y=reshape(Rx(ii,jj,1:NR),NR,1);
        R0=R0+y*y';
     end
     R=R+R0./N;
@@ -97,7 +108,7 @@ figure(1);
 % xlabel('velocity(m/s)'); % 设置x轴标签
 % ylim([10^-3,1]); % 设置y轴的显示范围
 
-plot(linspace(-60,60),abs(P_music_theta)/max(abs(P_music_theta))); % 绘制角度估计的MUSIC谱
+plot(linspace(-30,30),abs(P_music_theta)/max(abs(P_music_theta))); % 绘制角度估计的MUSIC谱
 ylabel('Pmusic(\theta)'); % 设置y轴标签
 xlabel('\theta(°)'); % 设置x轴标签
 ylim([10^-3,1]); % 设置y轴的显示范围
@@ -110,3 +121,47 @@ savefig('fig/figure3.fig'); % 保存图像到指定文件夹
 % fprintf('Range = %f\n',range); % 打印距离估计结果
 % fprintf('Velocity = %f\n',velocity); % 打印速度估计结果
 
+%% 4  Periodogram/FFT-based range-doppler estimation
+G=RxData./TxData;
+
+Kp=2^(ceil(log2(M)));% tau->range
+Mp=2^(ceil(log2(Fp*N)));% doppler->velocity
+P_TauDoppler=ifft( fft(G,Mp,2) , Kp , 1);%P_TauDoppler(tau,doppler);
+P_TauDoppler=conj(P_TauDoppler).*P_TauDoppler;
+velocity=1:Mp;
+velocity=velocity.*c0./(2*fc*Ts*Mp);
+velocity=kron(ones(Kp,1),velocity);
+range=1:Kp;
+range=range.*c0./(2*delta_f*Kp);
+range=kron(ones(1,Mp),range.');
+f=figure(2);
+f.OuterPosition=[100,250,1400,500];
+subplot(1,3,1)
+mesh(range,velocity,P_TauDoppler);
+xlabel('range/m');
+ylabel('velocity/m·s^-^1');
+zlabel('P');
+xlim([0 80]);
+ylim([0 80]);
+view(90,0)
+title('velocity-P');
+
+subplot(1,3,2)
+mesh(range,velocity,P_TauDoppler);
+xlabel('range/m');
+ylabel('velocity/m·s^-^1');
+zlabel('P');
+xlim([0 80]);
+ylim([0 80]);
+view(0,0);
+title('range-P');
+
+subplot(1,3,3)
+mesh(range,velocity,P_TauDoppler);
+xlabel('range/m');
+ylabel('velocity/m·s^-^1');
+zlabel('P');
+xlim([0 80]);
+ylim([0 80]);
+title('range-velocity');
+view(0,90);
